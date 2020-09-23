defbe58dce6aa30ed56b23951a3c695b7fc49f6d
Esteban, Jesús <jesus.esteban@accenture.com>
2020-09-23 13:24:07 +0200

    Updates

---
+++ precache-manifest.20daa667bc1818bb08ac68ad1d7516ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4642689c71c11fcca79d3b48ffcbb635",
+    "url": "/strava-map/index.html"
+  },
+  {
+    "revision": "f64cbcb3fe6ad31a378c",
+    "url": "/strava-map/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0033f01602d1c3e5e98",
+    "url": "/strava-map/static/js/2.9df8ce7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/strava-map/static/js/2.9df8ce7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f64cbcb3fe6ad31a378c",
+    "url": "/strava-map/static/js/main.5a201b53.chunk.js"
+  },
+  {
+    "revision": "99f6e44527b7cfbb309a",
+    "url": "/strava-map/static/js/runtime-main.24e72838.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/strava-map/static/media/logo.5d5d9eef.svg"
+  }
+]);
